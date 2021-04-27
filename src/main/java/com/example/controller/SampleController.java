6692c9a373148935ea14242dca217fe0d4ed7c42
shinji0320 <shinji.blue.one@gmail.com>
2021-04-27 09:18:46 +0900

    fix1

---
+++ src/main/java/com/example/controller/SampleController.java
@@ -0,0 +1,14 @@
+
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SampleController {
+
+    @RequestMapping("/sample")
+    public String sample() {
+        return "sample";
+    }
+}
